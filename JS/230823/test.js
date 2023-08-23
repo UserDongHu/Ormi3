74851864acf7ca640b24393825a40db83cf819e1
KimDongHu <ehdgn1022@naver.com>
2023-08-23 20:27:14 +0900

    JS 1일차 블로그 정리

---
+++ JS/230823/test.js
@@ -0,0 +1,31 @@
+// window.alert('hello world');
+
+// 주석을 이렇게 작성합니다!
+
+// 변수 만드는 3가지 방법
+// 변수 선언
+// var a;
+// let b;
+// // 상수 변수를 만드는 방법
+// const c = 3;
+
+// // var와 let는 이런식으로 계속 수정이 가능하다!
+// // 변수 초기화
+// a = 3
+// a = 10
+
+// b = 4
+// b = 11
+
+// c = 10 하면 ERROR
+
+// 'use strict'; // 자바 스크립트를 엄격모드에서 실행
+// var a = 1;
+// a = 3; // 이미 만들어진 a라는 변수를 3으로 변경
+// c = 3; // 엄격모드에서는 에러
+
+const a = 1;
+console.log(typeof a)
+
+var 문자열 = 'lee ho jun'
+document.write(문자열[0])
