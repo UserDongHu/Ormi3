d620d0a1e7748e55fb9432ae2deae5769778f6a6
KimDongHu <ehdgn1022@naver.com>
2023-10-21 16:20:49 +0900

    DRF 라우터 나중에 꼭 다시 한번 찾아서 공부

--- Django/231020/router/book/urls.py
+++ Django/231020/router/book/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter # default router는 기능 많고요. simple router는 기능 적어요.
 from .views import BookViewSet
 
-router = DefaultRouter()
-router.register('', BookViewSet)
+router = DefaultRouter() # 어떤 라우터를 사용할건지 
+router.register('', BookViewSet) # ''주소로 가면 BookViewset으로 연결하겠다.
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+] 
